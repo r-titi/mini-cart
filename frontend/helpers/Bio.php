b8fd5ca25ef185da8ac4485c1f255a59f756e52e
r-titi <r.titi@syarah.com>
2022-04-06 10:26:12 +0300

    b & c methods

--- frontend/helpers/Bio.php
+++ frontend/helpers/Bio.php
@@ -6,4 +6,9 @@ use common\models\Product;
 use yii\web\NotFoundHttpException;
 
 class Bio {
+    private $_desc;
+
+    public function getDesc() {
+        return $this->_desc;
+    }
 }
--- frontend/helpers/Camo.php
+++ frontend/helpers/Camo.php
@@ -6,4 +6,19 @@ use common\models\Product;
 use yii\web\NotFoundHttpException;
 
 class Camo {
+    
+    private $_job;
+    private $_title;
+    
+    public function do_camo() {
+
+    }
+
+    public function getJob() {
+        return $this->_job;
+    }
+
+    public function getTitle() {
+        return $this->_title;
+    }
 }
